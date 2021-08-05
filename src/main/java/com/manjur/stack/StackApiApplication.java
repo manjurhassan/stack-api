6c94ac399edba9317cb83c026dbd01c842e177b9
manjurhassan <43235097+manjurhassan@users.noreply.github.com>
2021-08-05 15:20:17 +0530

    Initial commit

---
+++ src/main/java/com/manjur/stack/StackApiApplication.java
@@ -0,0 +1,13 @@
+package com.manjur.stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manjur/stack/StackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manjur.stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
